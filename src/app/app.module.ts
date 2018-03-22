c30e6a8592cb6fea564fcd7efe38c367f949c53f
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-03-22 13:02:10 +0100

    setup angular2 frontend with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
